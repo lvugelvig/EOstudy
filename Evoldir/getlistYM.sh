cf3da95ace8878b7207302dd64eae1a4f27c748e
Florence Debarre <flo@flokapik-2.local>
2017-05-01 15:08:19 +0200

    bugs in getlist corrected, added year in file names

--- Evoldir/getlistYM.sh
+++ Evoldir/getlistYM.sh
@@ -32,10 +32,10 @@ do
   # 4: remove hour at the end
   # 5: reformat date so that they all have same length
   # 6: add typeevent at the end of the line
-sed -e 's/<\/a>//g' -e 's/^.*">//g' -e "s/ .*$2/, $2/g" -e 's/ [0-9]\{2\}:[0-9]\{2\}//g' -e "s/$2  /$2 0/g" -e "s/$/, ${typeevent}/g" ${typeevent}_tmp.txt > ${typeevent}_${2}.txt
+sed -e 's/<\/a>//g' -e 's/^.*">//g' -e "s/ .*$2/, $2/g" -e 's/ [0-9]\{2\}:[0-9]\{2\}//g' -e "s/$2  /$2 0/g" -e "s/$/, ${typeevent}/g" ${typeevent}_tmp.txt > ${typeevent}_${1}-${2}.txt
 
   # Download the ads
-  cat ${typeevent}_${2}.txt | cut -d, -f1 | while read line
+  cat ${typeevent}_${1}-${2}.txt | cut -d, -f1 | while read line
   do
     echo $line
     curl "http://life.mcmaster.ca/~brian/evoldir/${typeevent}/${line}" > ads/${1}/${2}/${line}.txt
